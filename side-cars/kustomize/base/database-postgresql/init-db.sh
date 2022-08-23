cd56571b4e7d26c18ea858270002b1348430838d
Zvi Grinberg <zgrinber@redhat.com>
2022-08-23 16:13:55 +0300

    second leg of implementing generic postgresql database schema creation

--- side-cars/kustomize/base/database-postgresql/init-db.sh
+++ side-cars/kustomize/base/database-postgresql/init-db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-
+postgresqlSideCarDB=$(grep postgresqlSideCarDB= /tmp/annotations | awk -F = '{print $2}')
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE DATABASE $APP_DATABASE_NAME;
+	CREATE DATABASE $postgresqlSideCarDB;
 EOSQL
